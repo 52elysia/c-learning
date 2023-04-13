8810b26c0896c38053c92a9dda3fcd346179c77c
向 日和 <yq2533478067@outlook.com>
2023-04-13 18:04:17 +0800

    二分查找

---
+++ Project4/Project4/FileName.cpp
@@ -0,0 +1,106 @@
+#define _CRT_SECURE_NO_WARNINGS 1	
+#include<stdio.h>
+
+#include<string.h>
+
+#include<windows.h>
+
+#include<stdlib.h>
+
+
+
+
+
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 11;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left <= right)
+//	{
+//		int mid = (right + left) / 2;
+//		if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > k)
+//		{
+//			right = mid - 1;
+//		}
+//		else
+//		{
+//			printf("找到了，下标是%d\n", mid);
+//			break;
+//		}
+//	}
+//	if (left > right)
+//	{
+//		printf("找不到\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 1;
+//	int ret = 1;
+//	int n = 0;
+//	scanf("%d\n", &n);
+//	for(i = 1; i <= n;i++)
+//	{
+//		ret = ret * i;
+//	}
+//	printf("ret=%d\n", ret);
+//	return 0;
+//
+//}
+//int main()
+//{
+//	char arr1[] = "welcom to j3 222!";
+//	char arr2[] = "#################";
+//	int left = 0;
+//	int right = strlen(arr1)-1;
+//	
+//	while (left <= right)
+//	{
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//		printf("%s\n",arr2);
+//		Sleep(1000);
+//		system("cls");//清空屏幕 执行系统命令的一个函数cls
+//        left++;
+//		right--;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	char password[20] = { 0 };
+//	for (i=0;i<3;i++)
+//	{
+//		printf("请输入密码:>");
+//		scanf("%s",password);
+//		if (strcmp(password,"123456") == 0)
+//		{
+//			printf("登陆成功\n");
+//			break;
+//		}
+//		else
+//		{
+//			printf("密码错误\n");
+//		}
+//	}
+//	if (i == 3)
+//		printf("密码输入错误次数过多，退出程序\n");
+//
+//	return 0;
+//	
+//	
+//}
+
+			
+
+
+
