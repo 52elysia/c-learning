411faaa8a0d1dc1fefe64e8e212526356faffee5
向 日和 <yq2533478067@outlook.com>
2023-04-13 18:17:32 +0800

    二分查找

---
+++ learning3.cpp
@@ -0,0 +1,94 @@
+#define _CRT_SECURE_NO_WARNINGS 1	
+#include<stdio.h>
+#include<string.h>
+
+
+
+
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//
+//
+//int main()
+//{
+//	int sum = 0;
+//	int a = 1;
+//	int b = 1;
+//	sum = Add(a, b);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr1[] = "elysia";
+//	char arr2[] = "######";
+//	strcpy(arr2, arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+//int main()
+//{
+//	char arr1[] = "hello world";
+//	memset(arr1, '！', 5);
+//	printf("%s\n", arr1);
+//
+//	return 0;
+//                           
+//}
+//int MAX(int x,int y)//函数的创建
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//
+//}
+//int main()
+//{
+//	int max= 0;
+//	int a = 10;
+//	int b = 20;
+//	max=MAX(a, b);
+//	printf("max=%d\n", max);
+//	return 0;
+//}
+//void Swap(int x, int y)//交换的地址不同错误的交换不能完成任务
+//{
+//	int temp = 0;
+//	temp=x;
+//	x = y;
+//	y = temp;
+//}
+//int main()
+//{
+//	int a = 30;
+//	int b = 40;
+//	printf("a=%d b=%d\n", a, b);
+//	Swap(a, b);
+//	printf("a=%d b=%d\n", a, b);
+//	return 0;
+//}
+//void Swap(int* pa, int* pb)
+//{
+//	int temp = 0;
+//	temp = *pa;
+//	*pa = *pb;
+//	*pb = temp;
+//}
+//int main()
+//{
+//	int x = 10;
+//	int y = 20;
+//	printf("x=%d y=%d\n", x, y);
+//	Swap(&x,&y);
+//	printf("x=%d y=%d\n", x, y);
+//	return 0;
+//}
+
+
+
