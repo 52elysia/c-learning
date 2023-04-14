dd54f01d3d68e12f256c38efcf9a459ea00d2b2f
向 日和 <yq2533478067@outlook.com>
2023-04-14 17:49:11 +0800

    二分查找函数实现

--- FileName.cpp
+++ FileName.cpp
@@ -1,94 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS 1	
 #include<stdio.h>
-#include<string.h>
+int main()
+{
+	
+	printf("hello world!");
+	return 0;
+}
 
-
-
-
-//int Add(int x, int y)
+//int binary_search(int arr[], int k,int sz)
 //{
-//	int z = 0;
-//	z = x + y;
-//	return z;
+//	//int sz = sizeof(arr) / sizeof(arr[0]);//不能用算法算数组的大小！
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left<=right)
+//	{
+//		int mid = (right - left) / 2;
+//		if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > k)
+//		{
+//			right = mid - 1;
+//		}
+//		else
+//		{
+//			return mid;
+//		}
+//	}
+//	return -1;
 //}
+//
 //
 //
 //int main()
 //{
-//	int sum = 0;
-//	int a = 1;
-//	int b = 1;
-//	sum = Add(a, b);
-//	printf("%d\n", sum);
-//	return 0;
-//}
-
-//int main()
-//{
-//	char arr1[] = "elysia";
-//	char arr2[] = "######";
-//	strcpy(arr2, arr1);
-//	printf("%s\n", arr2);
-//	return 0;
-//}
-//int main()
-//{
-//	char arr1[] = "hello world";
-//	memset(arr1, '！', 5);
-//	printf("%s\n", arr1);
-//
-//	return 0;
-//                           
-//}
-//int MAX(int x,int y)//函数的创建
-//{
-//	if (x > y)
-//		return x;
+//	int sz = 0;
+//	int k = 7;
+//	int arr[] = { 1,2,3,4,5,6,7,8 };
+//	sz = sizeof(arr) / sizeof(arr[0]);
+//	int ret = binary_search(arr,k,sz);
+//	if (ret == -1)
+//	{
+//		printf("找不到\n");
+//	}
 //	else
-//		return y;
-//
-//}
-//int main()
-//{
-//	int max= 0;
-//	int a = 10;
-//	int b = 20;
-//	max=MAX(a, b);
-//	printf("max=%d\n", max);
+//	{
+//		printf("找到了%d\n",ret);
+//	}
 //	return 0;
 //}
-//void Swap(int x, int y)//交换的地址不同错误的交换不能完成任务
-//{
-//	int temp = 0;
-//	temp=x;
-//	x = y;
-//	y = temp;
-//}
-//int main()
-//{
-//	int a = 30;
-//	int b = 40;
-//	printf("a=%d b=%d\n", a, b);
-//	Swap(a, b);
-//	printf("a=%d b=%d\n", a, b);
-//	return 0;
-//}
-//void Swap(int* pa, int* pb)
-//{
-//	int temp = 0;
-//	temp = *pa;
-//	*pa = *pb;
-//	*pb = temp;
-//}
-//int main()
-//{
-//	int x = 10;
-//	int y = 20;
-//	printf("x=%d y=%d\n", x, y);
-//	Swap(&x,&y);
-//	printf("x=%d y=%d\n", x, y);
-//	return 0;
-//}
-
-
-
